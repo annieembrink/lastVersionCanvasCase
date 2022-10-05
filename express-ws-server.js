4ad3279633600ef1968d5090c3acccdf0eb0dd68
anniesandberg <90192959+anniesandberg@users.noreply.github.com>
2022-10-05 09:44:04 +0200

    test

---
+++ express-ws-server.js
@@ -0,0 +1,116 @@
+/* dependencies - imports
+------------------------------- */
+import express from "express";
+
+// core module http - no npm install...
+import http from "http";
+
+// use websocket server
+import { WebSocketServer } from "ws";
+
+// import functions
+import { parseJSON, broadcast, broadcastButExclude } from "./libs/functions.js";
+
+
+
+/* application variables
+------------------------------- */
+// set port number >>> make sure client javascript uses same WebSocket port!
+const port = 80; 
+
+
+
+/* express
+------------------------------- */
+// express 'app' environment
+const app = express();
+
+// serve static files - every file in folder named 'public'
+app.use(express.static("public"));
+
+
+
+/* server(s)
+------------------------------- */
+// use core module http and pass express as an instance
+const server = http.createServer(app);
+
+// create WebSocket server - use a predefined server
+const wss = new WebSocketServer({ noServer: true });
+
+
+
+/* allow websockets - listener
+------------------------------- */
+// upgrade event - websocket communication
+server.on("upgrade", (req, socket, head) => {
+    console.log("Upgrade event client: ", req.headers);
+
+    // use authentication - only logged in users allowed ?
+    // socket.write('HTTP/1.1 401 Unauthorized\r\nWWW-Authenticate: Basic\r\n\r\n');
+    // socket.destroy();
+    // return;
+
+    // start websocket
+    wss.handleUpgrade(req, socket, head, (ws) => {
+        console.log("let user use websocket...");
+
+        wss.emit("connection", ws, req);
+    });
+});
+
+
+
+/* listen on new websocket connections
+------------------------------- */
+wss.on("connection", (ws) => {
+    console.log("New client connection from IP: ", ws._socket.remoteAddress);
+    console.log("Number of connected clients: ", wss.clients.size);
+
+    // WebSocket events (ws) for single client
+
+    // close event
+    ws.on("close", () => {
+        console.log("Client disconnected");
+        console.log(
+            "Number of remaining connected clients: ",
+            wss.clients.size
+        );
+    });
+
+    // message event
+    ws.on("message", (data) => {
+        // console.log('Message received: %s', data);
+
+        let obj = parseJSON(data);
+
+        // todo
+        // use obj property 'type' to handle message event
+        switch (obj.type) {
+            case "text":
+                break;
+            case "somethingelse":
+                break;
+            default:
+                break;
+        }
+
+        // message to clients
+        let objBroadcast = {
+            type: "text",
+            msg: obj.msg,
+            nickname: obj.nickname,
+        };
+
+        // broadcast to all but this ws...
+        broadcastButExclude(wss, ws, objBroadcast);
+    });
+});
+
+
+
+/* listen on initial connection
+------------------------------- */
+server.listen(port, (req, res) => {
+    console.log(`Express server (and http) running on port ${port}`);
+});
---
+++ public/js/code.js
@@ -0,0 +1,136 @@
+// DOM elements
+const inputText = document.getElementById("inputText");
+const setNickname = document.querySelector("#setNickname");
+
+
+// variable current user | nickname
+let nickname;
+
+
+// use WebSocket >>> make sure server uses same ws port!
+const websocket = new WebSocket("ws://localhost:80");  
+
+
+
+
+/* event listeners
+------------------------------- */
+
+// listen on close event (server)
+websocket.addEventListener("close", (event) => {
+    // console.log('Server down...', event);
+    document.getElementById("status").textContent = "Sry....server down";
+});
+
+// listen to messages from client | server
+websocket.addEventListener("message", (event) => {
+    // console.log(event.data);
+
+    let obj = parseJSON(event.data);
+
+    // todo
+    // use obj property 'type' to handle message event
+    switch (obj.type) {
+        case "text":
+            break;
+        case "somethingelse":
+            break;
+        default:
+            break;
+    }
+
+    // ...
+    renderMessage(obj);
+});
+
+setNickname.addEventListener("click", () => {
+    // get value from input nickname
+    nickname = document.getElementById("nickname").value;
+
+    // if set - disable input nickname
+    document.getElementById("nickname").setAttribute("disabled", true);
+
+    // enable input field
+    document.getElementById("inputText").removeAttribute("disabled");
+
+    // focus input field
+    document.getElementById("inputText").focus();
+});
+
+inputText.addEventListener("keydown", (event) => {
+    // press Enter...make sure at least one char
+    if (event.key === "Enter" && inputText.value.length > 0) {
+        // chat message object
+        let objMessage = {
+            msg: inputText.value,
+            nickname: nickname,
+        };
+
+        // show new message for this user
+        renderMessage(objMessage);
+
+        // send to server
+        websocket.send(JSON.stringify(objMessage));
+
+        // reset input field
+        inputText.value = "";
+    }
+});
+
+
+
+
+
+/* functions...
+------------------------------- */
+
+
+/**
+ * parse JSON
+ *
+ * @param {*} data
+ * @return {obj}
+ */
+function parseJSON(data) {
+    // try to parse json
+    try {
+        let obj = JSON.parse(data);
+
+        return obj;
+    } catch (error) {
+        // log to file in real application....
+        return { error: "An error receving data...expected json format" };
+    }
+}
+
+/**
+ * render new message
+ *
+ * @param {obj}
+ */
+function renderMessage(obj) {
+    // use template - cloneNode to get a document fragment
+    let template = document.getElementById("message").cloneNode(true);
+
+    // access content
+    let newMsg = template.content;
+
+    // change content...
+    newMsg.querySelector("span").textContent = obj.nickname;
+    newMsg.querySelector("p").textContent = obj.msg;
+
+    // new date object
+    let objDate = new Date();
+
+    // visual: 10:41 .. 9:5 ... leading zero....
+    newMsg.querySelector("time").textContent =
+        objDate.getHours() + ":" + objDate.getMinutes();
+
+    // set datetime attribute - see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/time
+    newMsg
+        .querySelector("time")
+        .setAttribute("datetime", objDate.toISOString());
+
+    // render using prepend method - last message first
+    document.getElementById("conversation").prepend(newMsg);
+}
